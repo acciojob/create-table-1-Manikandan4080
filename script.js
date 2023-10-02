c62864af90cefdcbe130cde604a91a4979dfeaf2
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-02 11:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function insert_Row() {
 	const table = document.getElementById("sampleTable");
+    const firstChild = table.firstChild;
     const row = document.createElement("tr")
     row.innerHTML=`<td>New Cell1</td>
                     <td>New Cell2</td>`
-    table.prepend(row);
+    table.insertBefore(row, firstChild)
 }
